6bc2a739e6c6f9839b88b37e1a17669b70d16868
RD <richard.thericgroup@hotmail.com>
2017-09-23 17:32:03 +1000

    clock and test

--- app/components/clock.js
+++ app/components/clock.js
@@ -1,6 +1,16 @@
 var React = require('react');
 var Clock = React.createClass({
-
+    formatSeconds: function(totalSecondes) {
+        var seconds = totalSecondes % 60;
+        var minutes = Math.floor(totalSecondes/60);
+        if (seconds < 10) {
+            seconds = '0'+seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0'+minutes;
+        }
+        return `${minutes}:${seconds}`
+    },
     render() {
         return(
             <div>
