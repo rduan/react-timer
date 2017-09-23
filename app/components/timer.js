46801c579722832703c8bb6364bd6b803aad0f29
RD <richard.thericgroup@hotmail.com>
2017-09-23 18:38:30 +1000

    scss for clock

--- app/components/timer.js
+++ app/components/timer.js
@@ -1,11 +1,13 @@
 var React = require('react');
+var Clock = require('./clock');
 
 var Timer = React.createClass({
-
+//<Clock totalSeconds = {1000} />
     render() {
         return(
             <div>
                 timer down
+                <Clock totalSeconds = {1000} />
             </div>
         )
     }
