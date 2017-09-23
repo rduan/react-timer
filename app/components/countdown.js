c028334f8525cf59d68e9fda4fca9a46f20577b8
RD <richard.thericgroup@hotmail.com>
2017-09-23 15:35:11 +1000

    navigation

---
+++ app/components/countdown.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Countdown = React.createClass({
+
+    render() {
+        return(
+            <div>
+                count down
+            </div>
+        )
+    }
+})
+
+module.exports = Countdown;
