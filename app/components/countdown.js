25d26fe96e8a32aab1537641429a08c0cda02355
RD <richard.thericgroup@hotmail.com>
2017-09-24 18:03:05 +1000

    countdown

--- app/components/countdown.js
+++ app/components/countdown.js
@@ -5,12 +5,36 @@ var CountdownForm = require('./countdown-form');
 var Countdown = React.createClass({
 
     getInitialState(){
-        return {count:0}
+        return {
+            count:0,
+            countdownSatus: 'stopped'
+        };
     },
+    startTimer(){
+        this.timer = setInterval(()=>{
+            var newCount = this.state.count -1;
+            this.setState({
+                count: newCount >= 0? newCount : 0,
+            });
+        }, 1000);
+    },
+    componentDidUpdate(prevProps,prevState) {
+        if (this.state.countdownSatus !== prevState.countdownSatus) {
+            switch (this.state.countdownSatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+                default:
+                    break;
+            }
+        }
+    },
+
     handleSetCountdown(seconds){
         this.setState({
-            count: seconds
-        })
+            count: seconds,
+            countdownSatus: 'started'
+        });
     },
 
     render() {
