fbb1db28f4f5bda0f6a6a6230d52018f246b4ba2
RD <richard.thericgroup@hotmail.com>
2017-09-28 07:12:21 +1000

    countdown test and style

--- app/components/countdown.js
+++ app/components/countdown.js
@@ -85,6 +85,7 @@ var Countdown = React.createClass({
         };
         return(
             <div>
+                <h1 className="page-title">Countdown App</h1>
                 <Clock totalSeconds = {count} />
                 {/* <CountdownForm onSetCountdown={this.handleSetCountdown}/> */}
                 {renderControlArea()}
