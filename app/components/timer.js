afc97d446a02bb30553a7578ac9ed75b03a43e6c
RD <richard.thericgroup@hotmail.com>
2017-09-29 09:15:03 +1000

    timer and test

--- app/components/timer.js
+++ app/components/timer.js
@@ -10,10 +10,61 @@ var Timer = React.createClass({
             timerStatus: 'stopped'
         };
     },
-//<Clock totalSeconds = {1000} />
 
+    handleStart(){
+        let that = this;
+        this.timer = setInterval(()=>{
+            var newCount = that.state.count +1;
+            that.setState({
+                count: newCount ,
+            });            
+        }, 1000);
+    },
+
+    handleStop(){
+        this.setState({
+            count: 0,
+            //timerStatus: 'stopped'
+        })
+    },
+
+    handlePause(){
+        clearInterval(this.timer);
+        this.timer = undefined;
+        // this.setState({
+        //     timerStatus: 'paused'
+        // })
+        
+    },
+
+    //only handle count
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.timerStatus !== prevState.timerStatus) {
+            switch (this.state.timerStatus) {
+                case 'started':
+                    this.handleStart();
+                    break;
+                case 'stopped':
+                    this.handleStop();
+                    //break;
+                    //will need clearInterval in handlePause()
+                case 'paused':
+                    this.handlePause();
+                    break;
+            }
+        }
+    },
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    },
+
+    //handle timerStatus, but not count
     handleStatusChange(newTimerStatus){
-        console.log(newTimerStatus);
+        // console.log(newTimerStatus);
+        this.setState({
+            timerStatus: newTimerStatus
+        })
     },
 
     render() {
